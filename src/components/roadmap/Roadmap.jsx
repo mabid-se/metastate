5637084fd86b27c549c7c56114a3d8f6f128b35b
mabid-se <mabid.se@gmail.com>
2023-07-10 16:55:34 +0500

    fixed the border left for roadmap

--- src/components/roadmap/Roadmap.jsx
+++ src/components/roadmap/Roadmap.jsx
@@ -11,33 +11,33 @@ const Roadmap = () => {
       heading: "Q1 2023",
       description:
         "Project inception - The idea for Metastate is conceived and the core team is formed. Extensive research and analysis are counducted to shape the platforms vision and goals",
-      top: { lg: 170, xl: 213 },
+      top: { lg: 80, xl: 217 },
       left: 70,
-      paddingBottom: { lg: "8%", xl: "108px" },
+      height: { lg: "210px", xl: "240px" },
     },
     {
       heading: "Q2 2023",
       description:
         "Whitepaper and Tokenomics - The whitepaper is finalized, outlining the platform's architecture, functionalities, and tokenomics. The team conducts internal reviews and seeks feedback from industry experts.",
-      top: { lg: 95, xl: 135 },
+      top: { lg: 23, xl: 135 },
       left: 354,
-      paddingBottom: { lg: "6%", xl: "88px" },
+      height: { lg: "210px", xl: "240px" },
     },
     {
       heading: "Q3 2023",
       description:
         "Team Expansion and Partnerships - The core team expands with the addition of key talents across various domains. Partnerships with industry-leading organizations and blockchain experts are established to enhance the project's credibility and expertise.",
-      top: { lg: 10, xl: 35 },
+      top: { lg: "-30px", xl: 55 },
       left: 628,
-      paddingBottom: { lg: "5%", xl: "80px" },
+      height: { lg: "210px", xl: "240px" },
     },
     {
       heading: "Q4 2023",
       description:
         "Token Pre-sale and Public Sale - The Metastate Token (Mets) - In the fourth quarter of 2023, we are thrilled to launch the Metastate Token (METS) as the heart of our ecosystem. METS will serve as the primary utility token, powering transactions, staking, and accessing exclusive features within the Metastate platform.",
-      top: "-65px",
+      top: { lg: "-90px", xl: "-35px" },
       left: 903,
-      paddingBottom: { lg: "4%", xl: "78px" },
+      height: { lg: "210px", xl: "240px" },
     },
   ];
   const stepsData2 = [
@@ -45,33 +45,33 @@ const Roadmap = () => {
       heading: "Q1 2023",
       description:
         "Blockchain Integration - Metastate selects the Cardano blockchain as the underlying infrastructure due to its scalability, security, and sustainability features. Integration with the Cardano network begins, leveraging its advanced smart contract capabilities",
-      top: { lg: 160, xl: 213 },
+      top: { lg: 80, xl: 217 },
       left: 70,
-      paddingBottom: { lg: "3%", xl: "108px" },
+      height: { lg: "210px", xl: "240px" },
     },
     {
       heading: "Q2 2023",
       description:
         "MVP Development - The Minimum Viable Product (MVP) development commences, focusing on core functionalities such as property tokenization, ownership transfers, and staking mechanisms. User interface design and user experience optimization are prioritized during this phase",
-      top: { lg: 95, xl: 130 },
+      top: { lg: 23, xl: 135 },
       left: 354,
-      paddingBottom: { lg: "3%", xl: "88px" },
+      height: { lg: "210px", xl: "240px" },
     },
     {
       heading: "Q3 2023",
       description:
         "MVP Testing and Feedback - The MVP undergoes rigorous testing and auditing to ensure security, performance, and functionality. External beta testing is conducted to gather user feedback and refine the platform based on user insights.",
-      top: { lg: 25, xl: 65 },
+      top: { lg: "-30px", xl: 55 },
       left: 628,
-      paddingBottom: { lg: "5%", xl: "80px" },
+      height: { lg: "210px", xl: "240px" },
     },
     {
       heading: "Q4 2023",
       description:
         "Blockchain Integration - Metastate selects the Cardano blockchain as the underlying infrastructure due to its scalability, security, and sustainability features. Integration with the Cardano network begins, leveraging its advanced smart contract capabilities",
-      top: { lg: "-65px", xl: "-27px" },
+      top: { lg: "-90px", xl: "-35px" },
       left: 903,
-      paddingBottom: { lg: "4%", xl: "78px" },
+      height: { lg: "210px", xl: "240px" },
     },
   ];
   const stepsData3 = [
@@ -79,37 +79,38 @@ const Roadmap = () => {
       heading: "Q1 2024",
       description:
         "Public Launch - The Metastate platform is officially launched to the public. The platform opens for property tokenization, investment, and staking.",
-      top: { lg: 195, xl: 250 },
+      top: { lg: 80, xl: 217 },
       left: 70,
-      paddingBottom: { lg: "6%", xl: "108px" },
+      height: { lg: "210px", xl: "240px" },
     },
     {
       heading: "Q2 2024",
       description:
         "Platform Enhancement and Community Growth - Continuous enhancements and updates are made to the Metastate platform based on user feedback and market demands. Community engagement initiatives, educational resources, and partnerships are established to foster a growing and supportive user base",
-      top: 95,
+      top: { lg: 23, xl: 135 },
       left: 354,
-      paddingBottom: { lg: "2%", xl: "70px" },
+      height: { lg: "210px", xl: "240px" },
     },
     {
       heading: "Q3 2024",
       description:
         "Expansion and Integration - Metastate aims to expand its ecosystem by integrating with external payment systems, real estate platforms, and other blockchain networks. Strategic partnerships are formed to broaden the range of investment opportunities and enhance user convenience.",
-      top: { lg: 13, xl: 30 },
+
+      top: { lg: "-30px", xl: 55 },
       left: 628,
-      paddingBottom: { lg: "3%", xl: "70px" },
+      height: { lg: "210px", xl: "240px" },
     },
     {
       heading: "Q4 2024",
       description:
         "Blockchain Integration - Metastate selects the Cardano blockchain as the underlying infrastructure due to its scalability, security, and sustainability features. Integration with the Cardano network begins, leveraging its advanced smart contract capabilities",
-      top: "-40px",
+      top: { lg: "-90px", xl: "-35px" },
       left: 903,
-      paddingBottom: { lg: "2%", xl: "70px" },
+      height: { lg: "210px", xl: "240px" },
     },
   ];
 
-  const [stepsData, setRoadMapData] = useState(stepsData1);
+  const [stepsData, setRoadMapData] = useState(stepsData3);
   const [style, setStyle] = useState();
   let [backgroundSize, setbackgroundSize] = useState(0);
 
@@ -123,7 +124,6 @@ const Roadmap = () => {
         ),
       []
     );
-
     useEffect(() => {
       observer.observe(ref.current);
       return () => {
@@ -133,7 +133,6 @@ const Roadmap = () => {
     return isIntersecting;
   }
 
-
   useEffect(() => {
     window.addEventListener(
       "wheel",
@@ -141,7 +140,7 @@ const Roadmap = () => {
         const st = window.pageYOffset || document.documentElement.scrollTop;
         const direction_1 = event.deltaY;
 
-        if (ref1.current.offsetTop >= st && ref1.current.offsetTop < st +100) {
+        if (ref1.current.offsetTop >= st && ref1.current.offsetTop < st + 100) {
           if (direction_1 > 0) {
             setbackgroundSize((backgroundSize += 3));
             setStyle({
@@ -156,7 +155,6 @@ const Roadmap = () => {
             } else if (backgroundSize > 200) {
               setRoadMapData(stepsData3);
             }
-
             if (backgroundSize > 300) {
               document.body.style.overflow = "auto";
               return;
@@ -353,14 +351,14 @@ const Roadmap = () => {
                         flexDirection: "column",
                         justifyContent: "flex-start",
                         alignItems: "flex-start",
-                        borderLeft: "2px solid #f2f2f2",
-                        borderImage: `linear-gradient(180deg, #0092FF 24%,  #42E3FF 60%, #fff 100%) 1`,
                         width: "245px",
-                        padding: "5px",
+                        height: item.height,
                         position: "absolute",
                         top: item.top,
                         left: item.left,
-                        paddingBottom: item.paddingBottom,
+                        borderLeft: "2px solid #f2f2f2",
+                        borderImage: `linear-gradient(180deg, #0092FF 24%,  #42E3FF 60%, #fff 100%) 1`,
+                        paddingLeft: "5px",
                       }}
                     >
                       <Typography
